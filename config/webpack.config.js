56488ccbf6b7555a68ab1522155159b384122479
David Pordomingo <David.Pordomingo.F@gmail.com>
2016-11-15 13:21:27 +0100

    Adding support to Flux/Fetch/Promise

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -35,5 +35,9 @@ module.exports = {
     },
     plugins: [
         extractSCSS,
+        new webpack.ProvidePlugin({
+            Promise: 'imports?this=>global!exports?global.Promise!es6-promise',
+            fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        })
     ]
 };
